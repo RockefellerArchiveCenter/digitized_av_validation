807359da32d58f304ba122657bdef3f479db8354
Hillel Arnold <hillel.arnold@gmail.com>
2023-03-14 11:43:06 -0400

    build out logic and add tests

--- test_validate.py
+++ test_validate.py
@@ -1,17 +1,238 @@
-import os
+import random
+from pathlib import Path
+from shutil import copyfile, copytree, rmtree
+from unittest.mock import patch
 
-from validate import Validator
+import bagit
+import boto3
+import pytest
+from moto import mock_s3
+
+from validate import AssetValidationError, Validator
+
+DEFAULT_ARGS = [
+    'audio',
+    'foo',
+    'bar',
+    'b90862f3baceaae3b7418c78f9d50d52.tar.gz',
+    'tmp']
+
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+    """Fixture to create and tear down tmp dir before and after a test is run"""
+    tmp_dir = Path(DEFAULT_ARGS[4])
+    if not tmp_dir.is_dir():
+        tmp_dir.mkdir()
+
+    yield  # this is where the testing happens
+
+    rmtree(DEFAULT_ARGS[4])
 
 
 def test_init():
-    """Asserts Validator init method sets attributes based on environment variables."""
-    source_bucket = "foo"
-    destination_bucket = "bar"
-    source_filename = "barz"
-    os.environ['SOURCE_BUCKET'] = source_bucket
-    os.environ['DESTINATION_BUCKET'] = destination_bucket
-    os.environ['SOURCE_FILENAME'] = source_filename
-    validator = Validator()
-    assert validator.source_bucket == source_bucket
-    assert validator.destination_bucket == destination_bucket
-    assert validator.source_filename == source_filename
+    """Asserts Validator init method sets attributes correctly."""
+    validator = Validator(*DEFAULT_ARGS)
+    assert validator.format == 'audio'
+    assert validator.source_bucket == 'foo'
+    assert validator.destination_bucket == 'bar'
+    assert validator.source_filename == 'b90862f3baceaae3b7418c78f9d50d52.tar.gz'
+    assert validator.tmp_dir == 'tmp'
+    assert validator.refid == 'b90862f3baceaae3b7418c78f9d50d52'
+
+    invalid_args = ['text', 'foo', 'bar', 'baz.tar.gz', 'tmp']
+    with pytest.raises(Exception):
+        Validator(*invalid_args)
+
+
+@patch('validate.Validator.download_bag')
+@patch('validate.Validator.extract_bag')
+@patch('validate.Validator.validate_bag')
+@patch('validate.Validator.validate_assets')
+@patch('validate.Validator.validate_file_formats')
+@patch('validate.Validator.move_to_destination')
+@patch('validate.Validator.cleanup_successful_job')
+@patch('validate.Validator.deliver_success_notification')
+def test_run(mock_deliver, mock_cleanup, mock_move, mock_validate_formats,
+             mock_validate_assets, mock_validate_bag, mock_extract_bag, mock_download):
+    """Asserts correct methods are called by run method."""
+    validator = Validator(*DEFAULT_ARGS)
+    extracted_path = Path(validator.tmp_dir, validator.refid)
+    download_path = "foo"
+    mock_download.return_value = download_path
+    validator.run()
+    mock_deliver.assert_called_once_with()
+    mock_cleanup.assert_called_once_with(extracted_path)
+    mock_move.assert_called_once_with(extracted_path)
+    mock_validate_formats.assert_called_once_with(extracted_path)
+    mock_validate_assets.assert_called_once_with(extracted_path)
+    mock_validate_bag.assert_called_once_with(extracted_path)
+    mock_extract_bag.assert_called_once_with(download_path)
+    mock_download.assert_called_once_with()
+
+
+@patch('validate.Validator.download_bag')
+@patch('validate.Validator.cleanup_failed_job')
+@patch('validate.Validator.deliver_failure_notification')
+def test_run_with_exception(mock_deliver, mock_cleanup, mock_download):
+    """Asserts run method handles exceptions correctly."""
+    validator = Validator(*DEFAULT_ARGS)
+    exception = Exception("Error downloading bag.")
+    mock_download.side_effect = exception
+    validator.run()
+    mock_cleanup.assert_called_once()
+    mock_deliver.assert_called_once_with(exception)
+
+
+@mock_s3
+def test_download_bag():
+    """Asserts file is downloaded correctly."""
+    validator = Validator(*DEFAULT_ARGS)
+    bucket_name = validator.source_bucket
+    expected_path = Path(validator.tmp_dir, validator.source_filename)
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.create_bucket(Bucket=bucket_name)
+    s3.put_object(Bucket=bucket_name, Key=validator.source_filename, Body='')
+
+    downloaded = validator.download_bag()
+    assert downloaded == expected_path
+    assert expected_path.is_file()
+
+
+def test_extract_bag():
+    """Asserts bag is extracted correctly and downloaded file is removed."""
+    validator = Validator(*DEFAULT_ARGS)
+    fixture_path = Path("fixtures", "b90862f3baceaae3b7418c78f9d50d52.tar.gz")
+    tmp_path = Path(validator.tmp_dir, validator.source_filename)
+    copyfile(fixture_path, tmp_path)
+
+    validator.extract_bag(tmp_path)
+    assert Path(validator.tmp_dir, validator.refid).is_dir()
+    assert not tmp_path.is_file()
+
+
+def test_validate_bag():
+    """Asserts bag validation is successful or raises expected exceptions on failure."""
+    validator = Validator(*DEFAULT_ARGS)
+    fixture_path = Path("fixtures", "b90862f3baceaae3b7418c78f9d50d52")
+    tmp_path = Path(validator.tmp_dir, validator.refid)
+    copytree(fixture_path, tmp_path)
+
+    validator.validate_bag(tmp_path)
+
+    rmtree(Path(tmp_path, 'data'))
+    with pytest.raises(bagit.BagValidationError):
+        validator.validate_bag(tmp_path)
+
+
+def test_validate_assets():
+    moving_image_args = ['moving image',
+                         'foo',
+                         'bar',
+                         '20f8da26e268418ead4aa2365f816a08.tar.gz',
+                         'tmp']
+    for args in [DEFAULT_ARGS, moving_image_args]:
+        validator = Validator(*args)
+        fixture_path = Path("fixtures", validator.refid)
+        tmp_path = Path(validator.tmp_dir, validator.refid)
+        copytree(fixture_path, tmp_path)
+
+        validator.validate_assets(tmp_path)
+
+
+def test_validate_assets_missing_file():
+    moving_image_args = ['moving image',
+                         'foo',
+                         'bar',
+                         '20f8da26e268418ead4aa2365f816a08.tar.gz',
+                         'tmp']
+    for args in [DEFAULT_ARGS, moving_image_args]:
+        validator = Validator(*args)
+        fixture_path = Path("fixtures", validator.refid)
+        tmp_path = Path(validator.tmp_dir, validator.refid)
+        copytree(fixture_path, tmp_path)
+
+        files = list(tmp_path.glob('data/*'))
+        random.choice(files).unlink()
+
+        with pytest.raises(AssetValidationError):
+            validator.validate_assets(tmp_path)
+
+
+def test_validate_file_formats():
+    pass
+
+
+@mock_s3
+def test_move_to_destination():
+    """Asserts correct file are moved to correct location."""
+    validator = Validator(*DEFAULT_ARGS)
+    fixture_path = Path("fixtures", "b90862f3baceaae3b7418c78f9d50d52")
+    tmp_path = Path(validator.tmp_dir, validator.refid)
+    copytree(fixture_path, tmp_path)
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.create_bucket(Bucket=validator.destination_bucket)
+
+    validator.move_to_destination(tmp_path)
+    expected_paths = [
+        f"{validator.refid}/{validator.refid}_a.mp3",
+        f"{validator.refid}/{validator.refid}_ma.wav"]
+    found = [o['Key'] for o in s3.list_objects_v2(
+        Bucket=validator.destination_bucket,
+        Prefix=validator.refid)['Contents']]
+    assert len(expected_paths) == len(found)
+    assert sorted(expected_paths) == sorted(found)
+
+
+@mock_s3
+def test_cleanup_successful_job():
+    """Asserts that successful jobs are cleaned up properly."""
+    validator = Validator(*DEFAULT_ARGS)
+    fixture_path = Path("fixtures", "b90862f3baceaae3b7418c78f9d50d52")
+    tmp_path = Path(validator.tmp_dir, validator.refid)
+    copytree(fixture_path, tmp_path)
+
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.create_bucket(Bucket=validator.source_bucket)
+    s3.put_object(
+        Bucket=validator.source_bucket,
+        Key=validator.source_filename,
+        Body='')
+
+    validator.cleanup_successful_job(tmp_path)
+    assert not tmp_path.is_dir()
+
+
+@mock_s3
+def test_cleanup_failed_job():
+    """Asserts that failed jobs are cleaned up properly."""
+    validator = Validator(*DEFAULT_ARGS)
+    fixture_path = Path("fixtures", "b90862f3baceaae3b7418c78f9d50d52")
+    tmp_path = Path(validator.tmp_dir, validator.refid)
+    copytree(fixture_path, tmp_path)
+
+    s3 = boto3.client('s3', region_name='us-east-1')
+    s3.create_bucket(Bucket=validator.destination_bucket)
+    s3.put_object(
+        Bucket=validator.destination_bucket,
+        Key=validator.refid,
+        Body='')
+    s3.put_object(Bucket=validator.destination_bucket,
+                  Key=f"{validator.refid}/foo", Body='')
+
+    validator.cleanup_failed_job(tmp_path)
+    assert not tmp_path.is_dir()
+    deleted = s3.list_objects(
+        Bucket=validator.destination_bucket,
+        Prefix=validator.refid).get('Contents', [])
+    assert len(deleted) == 0
+
+
+def test_deliver_success_notification():
+    pass
+    # TODO check on notification values
+
+
+def test_deliver_failure_notification():
+    pass
+    # TODO check on notification values
--- validate.py
+++ validate.py
@@ -1,11 +1,13 @@
 import os
+import tarfile
+from pathlib import Path
+from shutil import rmtree
+
+import bagit
+import boto3
 
 
 class ExtractError(Exception):
-    pass
-
-
-class BagValidationError(Exception):
     pass
 
 
@@ -17,45 +19,153 @@ class FileFormatValidationError(Exception):
     pass
 
 
-class NotificationError(Exception):
-    pass
-
-
 class Validator(object):
-    def __init__(self):
-        self.source_bucket = os.environ.get('SOURCE_BUCKET')
-        self.destination_bucket = os.environ.get('DESTINATION_BUCKET')
-        self.source_filename = os.environ.get('SOURCE_FILENAME')
+    """Validates digitized audio and moving image assets."""
+
+    def __init__(self, format, source_bucket,
+                 destination_bucket, source_filename, tmp_dir):
+        self.format = format
+        self.source_bucket = source_bucket
+        self.destination_bucket = destination_bucket
+        self.source_filename = source_filename
+        self.refid = Path(source_filename).stem.split('.')[0]
+        self.tmp_dir = tmp_dir
+        # TODO will need better instantiation here which passes credentials
+        self.eventbridge = boto3.client('events')
+        self.s3 = boto3.client('s3')
+        self.transfer_config = boto3.s3.transfer.TransferConfig(
+            multipart_threshold=1024 * 25,
+            max_concurrency=10,
+            multipart_chunksize=1024 * 25,
+            use_threads=True)
+        if self.format not in ['audio', 'moving image']:
+            raise Exception(f"Cannot process file with format {self.format}.")
+        if not Path(self.tmp_dir).is_dir():
+            raise Exception(f"Directory {self.tmp_dir} does not exist.")
 
     def run(self):
+        """Main method which calls all other logic."""
         try:
-            self.extract_bag()
-            self.validate_bag()
-            self.validate_assets()
-            self.validate_file_formats()
+            extracted = Path(self.tmp_dir, self.refid)
+            downloaded = self.download_bag()
+            self.extract_bag(downloaded)
+            self.validate_bag(extracted)
+            self.validate_assets(extracted)
+            self.validate_file_formats(extracted)
+            self.move_to_destination(extracted)
+            self.cleanup_successful_job(extracted)
             self.deliver_success_notification()
         except Exception as e:
+            self.cleanup_failed_job(extracted)
             self.deliver_failure_notification(e)
-            # TODO handle asset cleanup based on exception type
 
-    def extract_bag(self):
+    def download_bag(self):
+        """Downloads a streaming file from S3."""
+        downloaded_path = Path(self.tmp_dir, self.source_filename)
+        self.s3.download_file(
+            self.source_bucket,
+            self.source_filename,
+            downloaded_path,
+            Config=self.transfer_config)
+        return downloaded_path
+
+    def extract_bag(self, file_path):
+        """Extracts the contents of a TAR file."""
+        try:
+            tf = tarfile.open(file_path, "r:*")
+            tf.extractall(self.tmp_dir)
+            tf.close()
+            file_path.unlink()
+        except Exception as e:
+            raise ExtractError("Error extracting TAR file: {}".format(e))
+
+    def validate_bag(self, bag_path):
+        """Validates a bag.
+
+        Raises a bagit.BagValidationError with the error in the `details` property.
+        """
+        bag = bagit.Bag(str(bag_path))
+        bag.validate()
+
+    def validate_assets(self, bag_path):
+        """Ensures that all expected files are present."""
+        suffix_map = [
+            ("ma.wav", "Master"), ("a.mp3", "Access")] if self.format == 'audio' else [
+            ("ma.mov", "Master"), ("me.mov", "Mezzanine"), ("a.mp4", "Access")]
+        for suffix, filetype in suffix_map:
+            filename = f"{self.refid}_{suffix}"
+            if not Path(bag_path, 'data', filename).is_file():
+                raise AssetValidationError(
+                    f"{filetype} file {filename} missing.")
+
+    def validate_file_formats(self, bag_path):
+        """Ensures that files pass MediaConch validation rules."""
+        # validation_rules = {} if self.format == 'audio' else {}
         pass
 
-    def validate_bag(self):
+    def get_content_type(self, extension):
+        format_map = {
+            ".mov": "video/quicktime",
+            ".mp4": "video/mp4",
+            ".wav": "audio/x-wav",
+            ".mp3": "audio/mpeg"}
+        try:
+            return format_map[extension]
+        except KeyError:
+            raise Exception(
+                f"Unable to upload asset with unknown extension {extension}.")
+
+    def move_to_destination(self, bag_path):
+        """"Uploads directory to destination S3 bucket."""
+        for path_obj in bag_path.glob('data/*'):
+            self.s3.upload_file(
+                path_obj,
+                self.destination_bucket,
+                f"{self.refid}/{path_obj.name}",
+                ExtraArgs={
+                    'ContentType': self.get_content_type(
+                        path_obj.suffix)},
+                Config=self.transfer_config)
+
+    def cleanup_successful_job(self, bag_path):
+        """Removes artifacts from successful job."""
+        if bag_path.is_dir():
+            rmtree(bag_path)
+        self.s3.delete_object(
+            Bucket=self.source_bucket,
+            Key=self.source_filename)
+
+    def cleanup_failed_job(self, bag_path):
+        """Removes artifacts from failed job."""
+        if bag_path.is_dir():
+            rmtree(bag_path)
+        to_delete = self.s3.list_objects(
+            Bucket=self.destination_bucket,
+            Prefix=self.refid).get('Contents', [])
+        self.s3.delete_objects(
+            Bucket=self.destination_bucket,
+            Delete={'Objects': [{'Key': k['Key']} for k in to_delete]})
+
+    def deliver_success_notification(self):
+        """Sends notifications after successful run."""
+        # TODO: figure out notifications approach.
         pass
 
-    def validate_assets(self):
-        pass
-
-    def validate_file_formats(self):
-        pass
-
-    def deliver_success_notifications(self):
-        pass
-
-    def deliver_failure_notifications(self, exception):
+    def deliver_failure_notification(self, exception):
+        """"Sends notifications when run fails."""
+        # TODO: figure out notifications approach.
         pass
 
 
 if __name__ == '__main__':
-    Validator().run()
+    format = os.environ.get('FORMAT')
+    source_bucket = os.environ.get('SOURCE_BUCKET')
+    destination_bucket = os.environ.get('DESTINATION_BUCKET')
+    source_filename = os.environ.get('SOURCE_FILENAME')
+    tmp_dir = os.environ.get('TMP_DIR')
+    Validator(
+        format,
+        source_bucket,
+        destination_bucket,
+        source_filename,
+        tmp_dir).run()
