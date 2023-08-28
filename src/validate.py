2abc6666cdacb10d13ba6de93119fdc68c6a942d
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-28 19:00:59 -0400

    use windows line breaks

--- src/validate.py
+++ src/validate.py
@@ -309,7 +309,7 @@ class Validator(object):
             exception (Exception): the exception that was thrown.
         """
         client = self.get_client_with_role('sns', self.role_arn)
-        tb = '\n'.join(traceback.format_exception(exception))
+        tb = '\r\n'.join(traceback.format_exception(exception))
         client.publish(
             TopicArn=self.sns_topic,
             Message=f'{self.format} package {self.source_filename} is invalid',
@@ -332,7 +332,7 @@ class Validator(object):
                 },
                 'message': {
                     'DataType': 'String',
-                    'StringValue': f'{str(exception)}\n\n\n\n{tb}',
+                    'StringValue': f'{str(exception)}\r\n\r\n{tb}',
                 }
             })
         logging.debug('Failure notification sent.')
